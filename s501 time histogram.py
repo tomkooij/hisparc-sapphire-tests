18a9de6d2a614b2495d8593bb76c28ffe3c3c7ce
tomkooij <tomkooij@tomkooij.nl>
2014-09-29 22:20:29 +0200

    select events based on pulseheights

--- s501 time histogram.py
+++ s501 time histogram.py
@@ -9,7 +9,7 @@ Goal: recreate graphs form D.Pennink 2010
 ph1 > TRIGGER = charged particle
 ph1 < TRIGGER = gamma
 
-Find out value of TRIGGER (70mV ???? Read pennink 2010)
+Find out value of TRIGGER (<120 and >200 volgens LIO verslag)
 """
 
 import tables
@@ -21,7 +21,8 @@ START = datetime.datetime(2014,8,1)
 END = datetime.datetime(2014,9,1)
 FILENAME = 'station_501_augustus2014.h5'
 
-
+HIGH_PH = 200
+LOW_PH = 120
 
 #
 # Read event data from the ESD
@@ -51,12 +52,29 @@ def plot_dt_histogram(tA,tB,bins):
     dt = tA - tB
     
     # remove -1 and -999
-    fixed_dt = dt.compress((tA >= 0) & (tB >= 0))
+    fixed_dt = dt.compress((tA >= 0) & (tB >= 0) & (ph1 < 120) & (ph2 < 120))
 
-    hist(fixed_dt, bins=bins, histtype='step')
+    hist(fixed_dt, bins=bins)
+
 
     return True
 
+def fit_norm(tA,tB): 
+    dt = tA - tB
+    fixed_dt = dt.compress((tA>=0) & (tB>=0))
+    (mu, sigma) = scipy.stats.norm.fit(fixed_dt)
+#    print (mu, sigma)
+    return (mu, sigma)
+    
+
+
+
+#
+def plot_dt_histogram_ph(tA,tB):
+
+    
+    
+    return True
 
    
     
@@ -67,6 +85,8 @@ t1 = events.col('t1')
 t2 = events.col('t2')
 t3 = events.col('t3')
 t4 = events.col('t4')
+ph = events.col('pulseheights')
+
 ph1 = ph[:,0]
 ph2 = ph[:,1]
 ph3 = ph[:,2]
@@ -74,21 +94,23 @@ ph4 = ph[:,3]
     
 bins2ns5 = arange(-101.25,101.26,2.5)
     
-    # plot histgrams
-    #plot_dt_histogram(t1, t2, bins2ns5)
-    #plot_dt_histogram(t1, t3, bins2ns5)
-    #plot_dt_histogram(t1, t4, bins2ns5)
-    #plot_dt_histogram(t2, t3, bins2ns5)
-    #plot_dt_histogram(t2, t4, bins2ns5)
-#plot_dt_histogram(t3, t4, bins2ns5)
+#
+# Plot histogram for tA-tB using hardcoded event selection based on pulseheight
+#
     
- 
-def fit_norm(tA,tB): 
-    dt = tA - tB
-    fixed_dt = dt.compress((tA>=0) & (tB>=0))
-    (mu, sigma) = scipy.stats.norm.fit(fixed_dt)
-#    print (mu, sigma)
-    return (mu, sigma)
+dt = t1 - t2    
+    
+# remove -1 and -999
+# select events based on pulseheight    
+fixed_dt = dt.compress((t1 >= 0) & (t2 >= 0) & (ph1 < 120) & (ph2 < 120))
+print "number of events: %d" % len (fixed_dt)
+    
+hist(fixed_dt, bins=bins2ns5, normed=1)
+(mu, sigma) = scipy.stats.norm.fit(fixed_dt)
+        
+print "avg: %f, sigma: %f" % (mu,sigma)
 
+fitted_pdf = scipy.stats.norm.pdf(bins2ns5, mu, sigma)
+plot(bins2ns5, fitted_pdf)
 
 #data.close()
